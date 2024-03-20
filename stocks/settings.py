da8001e0d8b2549be6872b8e947a1ea4795f09ae
JRakau <josue.rakauskas@gmail.com>
2024-03-20 19:45:25 +0000

    er

--- stocks/settings.py
+++ stocks/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o2^(imhuw5$(@oy8va4ngl$$!$ycb_=lyqrxl4xk+kl2dzwk=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
