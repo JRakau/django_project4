94f03f11ac46d39264e510f8f4e13ed3354e639e
JRakau <josue.rakauskas@gmail.com>
2024-03-20 19:38:23 +0000

    r

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', view.stockPicker, name = 'stockPicker'),
+   path('', views.stockPicker, name = 'stockpicker'),
+    path('stocktracker/', views.stockTracker, name = 'stocktracker'),
 ]
