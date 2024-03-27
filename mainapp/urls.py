ca94f4bd05b750012ccc7e35cb87d5e995c69acf
JRakau <josue.rakauskas@gmail.com>
2024-03-27 17:27:42 +0000

    setup first main rotine
    Rotine that will get the data from API

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
    path('', views.stockPicker, name = 'stockpicker'),
+   path('stocktracker/', views.stockTracker, name = 'stocktracker'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,107 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+import requests
+
+
+from typing import List
+
+# dX4gER*E6GN!yEZ */
+
+
+api_key = "74fee5c279404d629efab122626502bb"
+
 
 # Create your views here
 def stockPicker(request):
     return render(request, 'mainapp/stockpicker.html')
+
+
+def stockTracker(request):
+   #
+
+    stockpicker = request.GET.getlist('stockpicker')
+    stockshare = str(stockpicker)[1:-1]
+
+    print(stockpicker)
+    for ticker_input in stockpicker:
+        print(get_stock_quote(ticker_input, api_key))
+
+    print(stockshare)
+
+    return render(request, 'mainapp/stocktracker.html')
+
+
+class MyStock:
+    """
+    My Stock class
+    """
+
+    def __init__(self, symbol, name, high_price):
+        # instance attribute
+        self.symbol = symbol
+        self.name = name
+        self.high_price = high_price
+
+
+def get_stock_quote(ticker, key):
+    """Get a quote for a given ticker symbol using API
+
+    Args:
+        ticker (String): AAPL Apple stocks or  MSFT Microsoft stocks
+        key (String): API Key to access the API
+
+    Returns:
+        _type_: This request will return JSON with the following structure
+        {
+            "symbol": "AAPL",
+            "name": "Apple Inc",
+            "exchange": "NASDAQ",
+            "mic_code": "XNAS",
+            "currency": "USD",
+            "datetime": "2021-09-16",
+            "timestamp": 1631772000,
+            "open": "148.44000",
+            "high": "148.96840",
+             "low": "147.22099",
+            "close": "148.85001",
+            "volume": "67903927",
+            "previous_close": "149.09000",
+            "change": "-0.23999",
+            "percent_change": "-0.16097",
+            "average_volume": "83571571",
+            "rolling_1d_change": "123.123",
+            "rolling_7d_change": "123.123",
+            "rolling_period_change": "123.123"
+            "is_market_open": false,
+            "fifty_two_week": {
+                "low": "103.10000",
+                "high": "157.25999",
+                "low_change": "45.75001",
+                "high_change": "-8.40999",
+                "low_change_percent": "44.37440",
+                "high_change_percent": "-5.34782",
+                "range": "103.099998 - 157.259995"
+            },
+            "extended_change": "0.09",
+            "extended_percent_change": "0.05",
+            "extended_price": "125.22",
+            "extended_timestamp": 1649845281
+            }
+    """
+    url = f"https://api.twelvedata.com/quote?symbol={ticker}&apikey={key}"
+    json_resp = 0
+    try:
+        json_resp = requests.get(url).json()
+        print("Request: " + json_resp)
+
+    except OSError as err:
+        print("OS error:", err)
+
+    except ValueError:
+        print("Could not convert data.")
+
+    except Exception as err:
+        print(f"Unexpected on get_stock_quote() {err=}, {type(err)=}")
+        print("Oops! Try again...")
+
+    return json_resp
